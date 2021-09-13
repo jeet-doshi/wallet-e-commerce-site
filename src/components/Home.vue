5db820c2f282f617318060f73d18b61e9cf5a41f
Jeet Doshi <jeetdoshi2003@gmail.com>
2021-09-13 21:05:08 +0530

    Home page updated

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,118 +1,532 @@
 <template>
+<body>
+<!-- Header Starts -->
 <div class="header">
     <div class="container">
-    <div class="navbar">
-        <div class="logo">
-            <img src="/Images/Logo.png">
-            <!-- <img src="" width="125px">   -->
-        </div>
+        <div class="navbar">
+            <div class="logo">
+                <img src="Images/logo-without-bg.png">
+            </div>
         <nav>
             <ul>
                 <li><a href="">Home</a></li>
                 <li><a href="">Products</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Contact</a></li>
-                <li><a href="">Account</a></li>
+                <li><a href="">About Us</a></li>
+                <!-- <li><a href="">  </a></li> -->
+                <!-- <li><a href="">Account</a></li> -->
             </ul>
         </nav>
-        <!-- <img src="/Images/Hornbull-Benial/Black" width="40px" height="40px"> -->
-    </div>
-    <div class="row">
-        <div class="col-2">
-            <h1>Shoe Wonder</h1>
-            <p>Give your workout a new style</p>
-            <a href="" class="btn">Explore Now </a>
-        </div>
-        <div class="col-2">
-            <!-- <img src="IMAGES/Images1/shoe.jpg"> -->
-        </div>
+        <img src="Images/Cart.png" class="cart">
+        <div style="margin-top: 50px; font-weight: bold;  background-color: #ba0e44;">  <h3 style=" background-color: #ba0e44;"> (0) </h3>  </div>
     </div>
     </div>
 </div>
+<!-- Header Ends -->
+
+<!-- Product Display Starts -->
+<div class="upperContainer">
+        <!-- Product One  -->
+       <div class="items">
+           <div class="disp_photo">
+               <img class="disp" src="Images/Horn-Rakhi-Gift-Hamper/Black.jpg   ">
+           </div>
+           <div class="choice">
+             <input class='red' name='color' type='radio' />
+             <input class='green' name='color' type='radio' />
+             <input checked class='yellow' name='color' type='radio' />
+             <input class='purple' name='color' type='radio' /> 
+           </div>
+           <h3>Hornbull</h3>
+           <span>Rakhi Gift Hamper</span>
+           <div class="discount">
+                <h2 style="margin-bottom:0px">₹2,632</h2>
+                <h4>₹3,695</h4>
+                
+           </div>
+           <button class="btn">Add To Cart</button>
+           <!-- <pre>Save ₹1,063 (29%) </pre> -->
+       </div>
+       <!-- Product Two -->
+       <div class="items">
+            <div class="disp_photo">
+                <img class="disp" src="Images/Hornbull-Benial/Black.jpg">
+            </div>
+            <div class="choice">
+                <input class='red' name='color' type='radio' />
+                <input class='green' name='color' type='radio' />
+            </div>
+            <h3>Hornbull</h3>
+            <span>Benial Men's wallet</span>
+           <div class="discount">
+                <h2>₹3,695</h2>
+                <h4 style="margin-bottom:0px">₹2,632</h4>
+           </div>
+           <button class="btn">Add To Cart</button>
+        </div>
+        <!-- Product Three -->
+        <div class="items">
+            <div class="disp_photo">
+                <img class="disp" src="Images/Hornbull-Billy/Black.jpg">
+            </div>
+            <div class="choice">
+                <input class='red' name='color' type='radio' />
+                <input class='green' name='color' type='radio' />
+            </div>
+            <h3>Hornbull</h3>
+            <span>Billy Men's wallet with pen and keychain</span>
+            <div class="discount temp">
+                <h2>₹3,695</h2>
+                <h4 style="margin-bottom:0px">₹2,632</h4>
+           </div>
+           <button class="btn">Add To Cart</button>
+        </div>
+        <!-- Product Four -->
+        <div class="items">
+            <div class="disp_photo">
+                <img class="disp" src="Images/Hornbull-Buttler/Black.jpg">
+            </div>
+            <div class="choice">
+                <input class='red' name='color' type='radio' />
+                <input class='green' name='color' type='radio' />
+                <input checked class='yellow' name='color' type='radio' />
+                <input class='purple' name='color' type='radio' /> 
+            </div>
+                <h3>Hornbull</h3>
+                <span>Buttler Men's wallet with pen and keychain</span>
+           <div class="discount">
+                <h2>₹3,695</h2>
+                <h4 style="margin-bottom:0px">₹2,632</h4>
+           </div>
+           <button class="btn">Add To Cart</button>
+        </div>
+        <!-- Product Five -->
+        <div class="items">
+            <div class="disp_photo">
+                <img class="disp" src="Images/Hornbull-Edward/Blue.jpg">
+            </div>
+            <div class="choice">
+                <input class='red' name='color' type='radio' />
+                <input class='green' name='color' type='radio' />
+                <input checked class='yellow' name='color' type='radio' />
+                <input class='purple' name='color' type='radio' /> 
+            </div>
+                <h3>Hornbull</h3>
+                <span>Edward Men's wallet</span>
+            <div class="discount">
+            <h2>₹3,695</h2>
+           <h4 style="margin-bottom:0px">₹2,632</h4>
+           </div>
+           <button class="btn">Add To Cart</button>
+    </div>
+    <!-- Product Six -->
+    <div class="items">
+        <div class="disp_photo">
+            <img class="disp" src="Images/Hornbull-label/Black.jpg">
+        </div>
+        <div class="choice">
+            <input class='red' name='color' type='radio' />
+            <input class='green' name='color' type='radio' />
+            <input checked class='yellow' name='color' type='radio' />
+            <input class='purple' name='color' type='radio' /> 
+            </div>
+        <h3>Hornbull</h3>
+        <span>Label Men's wallet with pen and keychain</span>
+           <div class="discount">
+           <h2>₹3,695</h2>
+           <h4 style="margin-bottom:0px">₹2,632</h4>
+           </div>
+           <button class="btn">Add To Cart</button>
+    </div>
+    <!-- Product Seven -->
+    <div class="items">
+        <div class="disp_photo">
+            <img class="disp" src="Images/Wildhorn-Carter/Black.jpg">
+        </div>
+        <div class="choice">
+            <input class='red' name='color' type='radio' />
+            <input class='green' name='color' type='radio' />
+            <input checked class='yellow' name='color' type='radio' />
+            <input class='purple' name='color' type='radio' /> 
+            </div>
+        <h3>Wildhorn</h3>
+        <span>Carter Men's wallet</span>
+           <div class="discount">
+           <h2>₹3,695</h2>
+           <h4 style="margin-bottom:0px">₹2,632</h4>
+           </div>
+           <button class="btn">Add To Cart</button>
+    </div>
+    <!-- Product Eight -->
+    <div class="items">
+        <div class="disp_photo">
+            <img class="disp" src="Images/Hornbull-Themes/Black.jpg">
+        </div>
+        <div class="choice">
+            <input class='red' name='color' type='radio' />
+            <input class='green' name='color' type='radio' />
+            <input checked class='yellow' name='color' type='radio' />
+            <input class='purple' name='color' type='radio' /> 
+            </div>
+        <h3>Hornbull</h3>
+        <span>Themes Men's Wallet</span>
+           <div class="discount">
+           <h2>₹17,595</h2>
+           <h4 style="margin-bottom:0px">₹13,548</h4>
+           </div>
+           <button class="btn">Add To Cart</button>
+    </div>
+</div> 
+<!-- Product Display Ends -->
+
+<!-- Footer Starts -->
+<div class="footer">
+<div class="container2">
+    <div class="footer_child footer_child_1">
+        <img src="Images/logo-without-bg.png" width="300px" height="100px">
+        <br />
+        <!-- <h1>Wallet Town</h1> -->
+    </div>
+    <div class="footer_child">
+        <ul>
+            <li class="first">Quick Links</li>
+            <li>Home</li>
+            <li>Products</li>
+            <li>About Us</li>
+            <li>Cart</li>
+        </ul>
+    </div>
+    <div class="footer_child">
+        <ul>
+            <li class="first">Contact Us</li>
+            <li>
+                29th Street, plot 304,
+                Washington, 410206 USA.
+            </li>
+            <li>+91-9087645364</li>
+            <li>enquiry@wallettown.com</li>
+        </ul>
+    </div>
+</div>
+</div>
+
+<!-- Footer Ends -->
+</body>
 
 </template>
 
-<style scoped>
-*
-{
+<style>
+* {
     margin: 0px;
     padding: 0px;
+    border: 0px;
     box-sizing: border-box;
+    background-color: #dff9f6;
 }
-body{
-    font-family: 'Libre Baskerville', serif;
+
+/* CSS FOR HEADER */
+.header{
+    background: #ba0e44;
+    height: 10%;
+    width: 100%;
+    color: #dff9f6;
+
+}
+
+.logo {
+    margin-left: 0px;
+    /* background-color: #ba0e44; */
 }
 .navbar{
     display: flex;
     align-items: center;
-    padding: 20px;
+    padding: 10px;
+    background-color: #ba0e44;
 }
 nav{
     flex: 1;
     text-align: right;
+     background-color: #ba0e44;
 }
 nav ul{
     display: inline-block;
     list-style-type: none;
+    justify-content: center;
+     background-color: #ba0e44;
+    
 }
 nav ul li{
     display: inline-block;
     margin-right: 20px;
+    background-color: #ba0e44;
+
 }
 a{
     text-decoration: none;
-    color: #555;
-    font-size: large;
+    color: #dff9f6;;
+    font-size: 25px;
+    padding: 20px;
+    background-color: #ba0e44;
 }
-p{
-    color: #555;
+
+a:hover {
+    background-color: white;
+    color: red;
+    border-radius: 18px;
 }
+
+.cart {
+     background-color: #ba0e44;
+}
+.cart:hover {
+    background-color: white;
+    border-radius: 18px;
+}
+
 .container{
-    max-width: 1300px;
+    max-width: 1700px;
     margin: auto;
     padding-left: 25px;
     padding-right: 25px;
+    background-color: #ba0e44;
 }
-.row{
+
+
+/* CSS FOR PRODUCT DISPLAY */
+.upperContainer{
+    width: 100%;
+    height:450px;
+    /* background-color:grey; */
     display: flex;
-    align-items: center;
     flex-wrap: wrap;
-    justify-content: space-around;
+    justify-content: space-evenly;
+    align-items: center;
+    /* color: black; */
+
 }
-.col-2{
-    flex-basis: 50%;
-    min-width: 300px;
+.items{
+    margin-top: 20px;
+    margin-bottom: 20px;
+    width:22%;
+    height: 550px;
+    border:solid black;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;   
 }
-.col-2 img{
-    max-width: 100%;
-    padding: 50px 0;
+.items:hover{
+    border-radius: 5% 5% 5% 5% / 5% 5% 5% 5% ;
+  box-shadow: 10px 10px rgba(0,0,0,.25);
+  box-shadow: 23px 23px rgba(0,0,0,.15);
+  transition: all .4s ease;
 }
-.col-2 h1{
-    font-size: 40px;
-    line-height: 60px;
-    margin: 25px 0;
+.disp_photo{
+    width: 90%;
+    height: 325px;
+    /* background-color: blue; */
+    margin-top: 20px;
+    /* margin-left: 15px; */
+    overflow: hidden;
+    align-items: flex-start;
+    object-fit: fill;
 }
-.col-2 img{
-    max-width: 100%;
-    padding: 20pxpx;
+.disp{
+    max-height:100%;
+    max-width:100%;
+    
 }
-.btn{
-    display: inline-block;
-    background: #FF523B;
-    color: #fff;
-    padding: 8px 30px;
-    margin: 30px 0;
+.choice{
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+}
+
+input[type='radio'] {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    width: 25px;
+    height: 25px;
+    margin: 5px 0 5px 5px;
+    background-size: 225px 70px;
+    position: relative;
+    float: left;
+    display: inline;
+    top: 0;
     border-radius: 30px;
-    transition: background 0.5s;
+    z-index: 99999;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000;
+  }
+  input[type='radio']:hover{
+    -webkit-filter: opacity(.4);
+    filter: opacity(.4);  
+  }
+  input[type="radio"]:checked{
+    -webkit-filter: opacity(.2);
+    filter: opacity(.2);  
+  }
+  .red{
+    background: red;
+  }
+  .red:checked{
+    background: linear-gradient(brown, red)
+  }
+  .green{
+    background: green;
+  }
+  .green:checked{
+    background: linear-gradient(green, lime);
+  }
+  .yellow{
+    background: yellow;
+  }
+  .yellow:checked{
+    background: linear-gradient(orange, yellow);
+  }
+  .purple{
+    background: purple;
+  }
+  .purple:checked{
+    background: linear-gradient(purple, violet);
+  }
+  .red:checked ~ img{
+    -webkit-filter: opacity(.5) drop-shadow(0 0 0 red);
+    filter: opacity(.5) drop-shadow(0 0 0 red);
+  }
+  .green:checked ~ img{
+    -webkit-filter: opacity(.5) drop-shadow(0 0 0 green);
+    filter: opacity(.5) drop-shadow(0 0 0 green);
+  }
+  .yellow:checked ~ img{
+    -webkit-filter: opacity(.5) drop-shadow(0 0 0 yellow);
+    filter: opacity(.5) drop-shadow(0 0 0 yellow);
+  }
+  .purple:checked ~ img{
+    -webkit-filter: opacity(.5) drop-shadow(0 0 0 purple);
+    filter: opacity(.5) drop-shadow(0 0 0 purple);
+  }
+  .disp_photo img{
+    width: 500px;
+    height: 700px;
+    position: relative;
+    object-fit: contain;
+  }
+  .label{
+    width: 150px;
+    height: 75px;
+    position: absolute;
+    top: 170px;
+    margin-left: 130px;
+  }
+  ::selection{
+    background: #000;
+  }
+  .follow{
+    color: #ccc;
+    font-family: 'Courier';
+    font-size: 18px;
+    font-weight: bold;
+    margin: 15px 5px 0 50px;
+    text-align: right;
+    text-shadow: 2px 2px 2px #000;
+  }
+  .follow a{
+    color: #ccc;
+    text-decoration: none;
+  }
+
+span {
+    color: black;
+}
+ 
+ span:hover{
+    color:blue;
+    cursor: pointer;
+}
+
+.btn{
+    height: 30px;
+    width:50%;
+    background-color: rgb(223, 223, 75);
+    border-radius: 8px;
+    font-size: medium;
+    margin-top: 10px;
 }
 .btn:hover{
-    background: #563434;
+    background-color: rgba(158, 160, 156, 0.2);
+    
 }
-.header{
-    background: radial-gradient(#fff,#FFD6D6);
+
+h4{
+        text-decoration: line-through;
 }
-.header .row{
-    margin-top: 115px;
+
+h2{
+   
+    height:30px;
+    color:red;
+}
+
+/* CSS FOR FOOTER */
+.container2{
+    display: flex;
+    width:100%;
+    background-color: DIMGREY;
+    margin-top: 20px;
+}
+.footer{
+    width: 100%;
+    height: 225px;
+    margin-top: 750px;
+    background-color: DIMGREY;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+.footer_child{
+    height: 200px;
+    width: 25%;
+    margin-left: 20px;
+    font-family: segoe UI;
+    display: flex;
+    justify-content: center;
+    background-color: DIMGREY;
+}
+.footer_child_1{
+    align-items: center;
+    color: white;
+    font-size: 30px;
+    background-color: DIMGREY;
+    flex-direction: column;
+}
+
+.footer_child_4{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: DIMGREY;
+}
+
+.first{
+    font-size: 22px;
+}
+
+.footer_child ul {
+    margin-top: 40px;
+    background-color: DIMGREY;
+}
+
+.footer_child li {
+    list-style-type: none;
+    margin-bottom: 10px;
+    color: white;
+    background-color:DIMGREY;
+}
+.footer_child li:hover{
+    color:orange;
+    cursor: pointer;
+
 }
 
 </style>
