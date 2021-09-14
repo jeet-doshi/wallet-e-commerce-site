04ca30ad24eeb86e3902a8bac26f7c137a3a6fe2
sohamdixit <sohamddixit@gmail.com>
2021-09-14 12:41:48 +0530

    updated checkout

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -32,22 +32,22 @@
                     <div class="col-50">
                         <h3>Billing Address</h3>
                         <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-                        <input type="text" id="fname" name="fullname" placeholder="Soeng.Souy" required>
+                        <input type="text" id="fname" name="fullname" placeholder="name surname" required>
                         <label for="email"><i class="fa fa-envelope"></i> Email</label>
-                        <input type="text" id="email" name="email" placeholder="soeng.souy@gmail.com" required>
+                        <input type="text" id="email" name="email" placeholder="name.surname@gmail.com" required>
                         <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-                        <input type="text" id="adr" name="address" placeholder="110 W. 15th Phnom Penh" required>
+                        <input type="text" id="adr" name="address" placeholder="address" required>
                         <label for="city"><i class="fa fa-institution"></i> City</label>
-                        <input type="text" id="city" name="city" placeholder="Phnom Penh" required>
+                        <input type="text" id="city" name="city" placeholder="city" required>
 
                         <div class="row">
                             <div class="col-50">
                                 <label for="state">State</label>
-                                <input type="text" id="state" name="state" placeholder="Phnom Penh" required>
+                                <input type="text" id="state" name="state" placeholder="state" required>
                             </div>
                             <div class="col-50">
                                 <label for="zip">Zip</label>
-                                <input type="text" id="zip" name="zip" placeholder="12000" required>
+                                <input type="text" id="zip" name="zip" placeholder="123456" required>
                             </div>
                         </div>
                     </div>
@@ -63,19 +63,19 @@
                         </div>
 
                         <label for="cname">Name on Card</label>
-                        <input type="text" id="cname" name="cardname" placeholder="Soeng Souy" required>
+                        <input type="text" id="cname" name="cardname" placeholder="name surname" required>
                         <label for="ccnum">Credit card number</label>
                         <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444" required>
                         <label for="expmonth">Exp Month</label>
-                        <input type="text" id="expmonth" name="expmonth" placeholder="September" required>
+                        <input type="text" id="expmonth" name="expmonth" placeholder="month" required>
                         <div class="row">
                             <div class="col-50">
                                 <label for="expyear">Exp Year</label>
-                                <input type="text" id="expyear" name="expyear" placeholder="2021" required>
+                                <input type="text" id="expyear" name="expyear" placeholder="year" required>
                             </div>
                             <div class="col-50">
                                 <label for="cvv">CVV</label>
-                                <input type="text" id="cvv" name="cvv" placeholder="352" required>
+                                <input type="text" id="cvv" name="cvv" placeholder="cvv" required>
                             </div>
                         </div>
                     </div>
@@ -90,12 +90,12 @@
     <div class="col-25">
         <div class="container">
             <h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> <b>4</b></span></h4>
-            <p><a href="#">IPHONE 12 Pro Mac</a> <span class="price">$1500</span></p>
-            <p><a href="#">SAMSUNG S21</a> <span class="price">$1500</span></p>
-            <p><a href="#">OPPO F14</a> <span class="price">$1400</span></p>
-            <p><a href="#">HUAWEI 20 Mac</a> <span class="price">$1200</span></p>
+            <p><a href="#">Hornbull</a> <span class="price">2500</span></p>
+            <p><a href="#">Benial Men's wallet</a> <span class="price">3500</span></p>
+            <p><a href="#">Billy Men's wallet with pen and keychain</a> <span class="price">4400</span></p>
+            <p><a href="#">Buttler Men's wallet with pen and keychain/a> <span class="price">5200</span></p>
             <hr>
-            <p>Total <span class="price" style="color:black"><b>$12600</b></span></p>
+            <p>Total <span class="price" style="color:black"><b>15600</b></span></p>
         </div>
     </div>
 </div>
